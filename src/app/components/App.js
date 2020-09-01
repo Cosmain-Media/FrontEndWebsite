27bb34958eaeaf86392861a8928ebd78c4f1a8bc
Ari He <ariqhe@gmail.com>
2020-09-01 10:58:01 -0700

    Changed the entry point to Main.js

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
+import Main from '../components/Main';
 
 function App() {
   return (
-    <div className="App">
-      This is App.js
-    </div>
+    // <div className="App">
+    //   This is App.js
+    // </div>
+      <Main/>
   );
 }
 
--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,1 +1,14 @@
 //This file is the entry point for the project
+import React, {Component} from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is Main.js</h1>
+            </div>
+        );
+    }
+}
+
+export default Main;
