d010539963792abc5efcc4eebcd26c6d58fe420f
Ari He <ariqhe@gmail.com>
2020-09-01 15:33:14 -0700

    Added a little code to SearchBar.js

--- src/app/components/BrowseArea/SearchBar.js
+++ src/app/components/BrowseArea/SearchBar.js
@@ -4,7 +4,27 @@ class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = props; // copy the props so we can handle the search logic here
+        this.state = this.createState(props); // copy the props so we can handle the search logic here
+
+    }
+
+    createState(props) {
+        let state = {
+            allCategories: props.allCategories,
+            allProfessionals: props.allProfessionals,
+            searchBarInputValue: '',
+        }
+
+        return state
+    }
+
+    searchBarFilterOnChange = (event) => {
+        console.log("searchBarFilterOnChange activated", event.target.value)
+
+        this.setState({
+            searchBarInputValue: event.target.value
+        })
+
     }
 
     render() {
