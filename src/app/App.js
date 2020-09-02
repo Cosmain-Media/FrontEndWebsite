67600ec587823fbd0253846d3217bf444011cd6a
Fabricio <contact@fabricioguardia.com>
2020-09-02 11:20:26 -0700

    nav component created

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom'
+import HomePage from '../pages/homepage'
 
 function App() {
   return (
-    <div className="App">
-      Hello world
+    <div>
+      <Switch>
+        <Route exat path="/" render={ () => 
+          <HomePage/>
+        }></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Nav from '../components/nav'
+
+class HomePage extends Component {
+    render () {
+        return (
+            <Nav />
+        )
+    }
+}
+export default HomePage
