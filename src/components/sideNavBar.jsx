050090387ad5ab782bb6dc3047b6038ec9023779
james <jamesjkjk@gmail.com>
2020-09-03 11:55:09 -0700

    Add basic side bar nav functionality

--- src/components/sideNavBar.jsx
+++ src/components/sideNavBar.jsx
@@ -4,27 +4,37 @@ class SideNavBar extends Component {
     constructor() {
         super()
         this.state = {
-           navDrop: [false,false,false]
+           navDrop: [false, false, false]
         }
       }
-    toggle = (int) => {
-        let navDrops = [...this.state.navDrop];
-        let navDrop = {...navDrops[int]};
-        
-    }
+    toggleNav = (int) => {
+        this.setState(state => {
+            const navDrop = state.navDrop.map((data, index) => {
+                if (index === int) {
+                    return !data;
+                } else {
+                    return data;
+                }
+            });
+            return {
+              navDrop
+            };
+          });
+          console.log(this.state.navDrop);
+        }
 
+
+       
     navDrop = (e) => {
-        if(e.target.id=="beauty"){
-            toggleNav(1);
+        if(e.target.id === "beauty"){
+            this.toggleNav(0);
         }
-        {/*
-        if(e.target.id=="beauty"){
-            toggleNav(2);
+        if(e.target.id=="wellness"){
+            this.toggleNav(1);
         }
-        if(e.target.id=="beauty"){
-            toggleNav(3);
+        if(e.target.id=="popular"){
+            this.toggleNav(2);
         }
-        */}
     }
     render () {
         return (
@@ -32,45 +42,72 @@ class SideNavBar extends Component {
                 <div className="side-sticky-menu">
                     <div className="content">
                         <div className="content-body">
-                            <div class="dropdown-btn" id="beauty" onClick={this.navDrop}>Beauty</div>
-                            <ui class="dropdown-container">
-                                <a className="list-item">
-                                    <li>Barber</li>
-                                </a>
-                                <a className="list-item">
-                                    <li>Cosmetic Doctor</li>
-                                </a>
-                                <a className="list-item">
-                                    <li>Cosmetic Registered Nurse</li>
-                                </a>
-                                <a className="list-item">
-                                    <li>Cosmetic Surgeon</li>
-                                </a>
-                                <a className="list-item">
-                                    <li>Esthetician</li>
-                                </a>
-                                <a className="list-item">
-                                    <li>Hair Stylist</li>
-                                </a>
-                                <a className="list-item">
-                                    <li>Lash Technician</li>
-                                </a>
-                                <a className="list-item">
-                                    <li>Makeup Artist</li>
-                                </a>
-                                <a className="list-item">
-                                    <li>Nail Artist</li>
-                                </a>
-                                <a className="list-item">
-                                    <li>Piercing Artist</li>
-                                </a>
-                                <a className="list-item">
-                                    <li>Tanning Artist</li>
-                                </a>
-                                <a className="list-item">
-                                    <li>Tattoo Artist</li>
-                                </a>
-                            </ui>
+                            <button className="dropdown-btn" id="beauty" onClick={this.navDrop}>Beauty</button>
+                            {this.state.navDrop.map(function(data, index){
+                                 if (index===0 && data ===true) {
+                                    return (<ul className="dropdown-container" key={index}>
+                                        <a className="list-item">
+                                            <li>Barber</li>
+                                        </a>
+                                        <a className="list-item">
+                                            <li>Cosmetic Doctor</li>
+                                        </a>
+                                        <a className="list-item">
+                                            <li>Cosmetic Registered Nurse</li>
+                                        </a>
+                                    </ul>
+                                    );
+                                } 
+                            })}  
+                            <button className="dropdown-btn" id="wellness" onClick={this.navDrop}>Wellness</button>
+                            {this.state.navDrop.map(function(data, index){
+                                 if (index===1 && data ===true) {
+                                    return (<ul className="dropdown-container" key={index}>
+                                        <a className="list-item">
+                                            <li>Acupuncturist</li>
+                                        </a>
+                                        <a className="list-item">
+                                            <li>Athletic Trainer</li>
+                                        </a>
+                                        <a className="list-item">
+                                            <li>Chiropractor</li>
+                                        </a>
+                                        <a className="list-item">
+                                            <li>Dietitian</li>
+                                        </a>
+                                        <a className="list-item">
+                                            <li>Fitness Instructor</li>
+                                        </a>
+                                    </ul>
+                                    );
+                                } 
+                            })}   
+                            <button className="dropdown-btn" id="popular" onClick={this.navDrop}>Popular Services</button>
+                            {this.state.navDrop.map(function(data, index){
+                                 if (index===2 && data ===true) {
+                                    return (<ul className="dropdown-container" key={index}>
+                                        <a className="list-item">
+                                            <li>Traditional Chinese Acupuncturist</li>
+                                        </a>
+                                        <a className="list-item">
+                                            <li>Acupuncture</li>
+                                        </a>
+                                        <a className="list-item">
+                                            <li>Balayage</li>
+                                        </a>
+                                        <a className="list-item">
+                                            <li>Body Waxing</li>
+                                        </a>
+                                        <a className="list-item">
+                                            <li>Couples Massage</li>
+                                        </a>
+                                        <a className="list-item">
+                                            <li>Dermal/Cosmetic Filters</li>
+                                        </a>
+                                    </ul>
+                                    );
+                                } 
+                            })}    
                         </div>
                     </div>
                 </div>
