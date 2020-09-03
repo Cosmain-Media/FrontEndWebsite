ae2cd464b0873303e31e78902a22c98a237b9c9d
sergior2 <sergioir@uci.edu>
2020-09-03 14:50:27 -0700

    Starting setup for component

---
+++ src/components/vidLayout.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class vidLayout extends Component {
+
+    render () {
+        return (
+            <div>
+
+            </div>     
+            
+        )
+    }
+}
+
+export default vidLayout
