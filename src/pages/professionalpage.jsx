9efc8079eebb28489f495a51a3328a5be001246f
james <jamesjkjk@gmail.com>
2020-09-09 16:52:18 -0700

    added professional page and section

---
+++ src/pages/professionalpage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Professional from '../components/professional'
+
+class ProfessionalPage extends Component {
+    render () {
+        return (
+            <div > 
+               < Professional />
+            </div>
+        )
+    }
+}
+export default ProfessionalPage
--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import SideNavBar from '../components/sideNavBar';
-import Professional from '../components/professional';
 
 class Test extends Component {
     render () {
         return (
             <div >
                 <SideNavBar />  
-                <Professional />
             </div>
         )
     }
