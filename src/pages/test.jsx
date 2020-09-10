62a2893c132c329ef21fdda1774c2790533d956a
sergior2 <sergioir@uci.edu>
2020-09-10 14:30:23 -0700

    Hero Section Setup Complete w/additional changes to other components for fluidity

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import SideNavBar from '../components/sideNavBar';
 import Professional from '../components/professional';
-import Footer from '../components/footer'
+import Footer from '../components/footer';
+import Carousel from '../components/header'
 
 class Test extends Component {
     render () {
         return (
             <div >
-                <SideNavBar />  
-                <Professional />
+                <Carousel />
                 <Footer />
             </div>
         )
