23192b7e28d9fe748f4fe586a27751ecc8d8436c
Preston <yaumanyoung@yahoo.com>
2020-09-17 15:49:17 -0700

    Initial Branch Commit

--- src/components/vidLayout.jsx
+++ src/components/vidLayout.jsx
@@ -24,24 +24,26 @@ class vidLayout extends Component {
                         Most Popular
                     </h1>
                     
-                    {this.state.videos.map( function(video,index){ return <div className="video-slidebar" key={index}>
-                        <div className="video-slidebar-container">
-                            <div className="video-slidebar-container-iframe">
-                                <iframe src={video.videoLink} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                            </div>
-                            <div className="video-slidebar-container-description">
-                                <span className="video-slidebar-container-description-students">{video.favorites}</span>
-                                <span className="video-slidebar-container-description-time">{video.duration}</span>
-                                <span className="video-slidebar-container-description-heading">{video.title}</span>
-                                <span className="video-slidebar-container-description-professor">{video.professionalID}</span>
-                                <span className="video-slidebar-container-description-bookmark">
-                                    <svg className="video-slidebar-container-description-bookmark-icon">
-                                        <use xlinkHref="/images/sprite.svg#icon-bookmark"></use>
-                                    </svg>
-                                </span>
-                            </div>
-                        </div>               
-                    </div>})}
+                    {
+                        console.log()
+                    // <div className="video-slidebar" key={index}>
+                    //     <div className="video-slidebar-container">
+                    //         <div className="video-slidebar-container-iframe">
+                    //             <iframe src={video.videoLink} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    //         </div>
+                    //         <div className="video-slidebar-container-description">
+                    //             <span className="video-slidebar-container-description-students">{video.favorites}</span>
+                    //             <span className="video-slidebar-container-description-time">{video.duration}</span>
+                    //             <span className="video-slidebar-container-description-heading">{video.title}</span>
+                    //             <span className="video-slidebar-container-description-professor">{video.professionalID}</span>
+                    //             <span className="video-slidebar-container-description-bookmark">
+                    //                 <svg className="video-slidebar-container-description-bookmark-icon">
+                    //                     <use xlinkHref="/images/sprite.svg#icon-bookmark"></use>
+                    //                 </svg>
+                    //             </span>
+                    //         </div>
+                    //     </div>               
+                    // </div>})}
 
                     <h1 className="video-heading">
                         Tutorials
