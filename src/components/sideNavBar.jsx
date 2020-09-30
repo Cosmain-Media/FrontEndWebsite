1681fbbada959a05cdf141a5a526cb5b3ce5aea7
sergior2 <sergioir@uci.edu>
2020-09-30 14:37:38 -0700

    Fixed onClick props to respond to div clicks

--- src/components/sideNavBar.jsx
+++ src/components/sideNavBar.jsx
@@ -48,14 +48,14 @@ class SideNavBar extends Component {
                             {this.state.navDrop.map(function(data, index){
                                  if (index===0 && data ===true) {
                                     return (<ul className="dropdown-container" key={index}>
-                                        <div className="list-item active">
-                                            <a onClick={ () => { changeProfessional('Barber') } } >Barber</a>
+                                        <div className="list-item active" onClick={ () => { changeProfessional('Barber') } }>
+                                            <a>Barber</a>
                                         </div>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Cosmetic Doctor') } } >Cosmetic Doctor</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Cosmetic Doctor') } }>
+                                            <a>Cosmetic Doctor</a>
                                         </div>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Cosmetic Registered Nurse') } } >Cosmetic Registered Nurse</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Cosmetic Registered Nurse') } }>
+                                            <a>Cosmetic Registered Nurse</a>
                                         </div>
                                     </ul>
                                     );
@@ -65,20 +65,20 @@ class SideNavBar extends Component {
                             {this.state.navDrop.map(function(data, index){
                                  if (index===1 && data ===true) {
                                     return (<ul className="dropdown-container" key={index}>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Acupuncturist') } } >Acupuncturist</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Acupuncturist') } }>
+                                            <a>Acupuncturist</a>
                                         </div>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Athletic Trainer') } } >Athletic Trainer</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Athletic Trainer') } }>
+                                            <a>Athletic Trainer</a>
                                         </div>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Chiropractor') } } >Chiropractor</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Chiropractor') } }>
+                                            <a>Chiropractor</a>
                                         </div>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Dietitian') } } >Dietitian</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Dietitian') } }>
+                                            <a>Dietitian</a>
                                         </div>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Fitness Instructor') } } >Fitness Instructor</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Fitness Instructor') } }>
+                                            <a>Fitness Instructor</a>
                                         </div>
                                     </ul>
                                     );
@@ -88,23 +88,23 @@ class SideNavBar extends Component {
                             {this.state.navDrop.map(function(data, index){
                                  if (index===2 && data ===true) {
                                     return (<ul className="dropdown-container" key={index}>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Traditional Chinese Acupuncturist') } } >Traditional Chinese Acupuncturist</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Traditional Chinese Acupuncturist') } }>
+                                            <a>Traditional Chinese Acupuncturist</a>
                                         </div>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Acupuncture') } } >Acupuncture</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Acupuncture') } }>
+                                            <a>Acupuncture</a>
                                         </div>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Balayage') } } >Balayage</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Balayage') } }>
+                                            <a>Balayage</a>
                                         </div>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Body Waxing') } } >Body Waxing</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Body Waxing') } }>
+                                            <a>Body Waxing</a>
                                         </div>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Couples Massage') } } >Couples Massage</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Couples Massage') } }>
+                                            <a>Couples Massage</a>
                                         </div>
-                                        <div className="list-item">
-                                            <a onClick={ () => { changeProfessional('Dermal/Cosmetic Filters') } } >Dermal/Cosmetic Filters</a>
+                                        <div className="list-item" onClick={ () => { changeProfessional('Dermal/Cosmetic Filters') } }>
+                                            <a>Dermal/Cosmetic Filters</a>
                                         </div>
                                     </ul>
                                     );
