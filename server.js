62965cb1673de97215cd8a62cc3e9f3608db9908
Fabricio <contact@fabricioguardia.com>
2020-10-15 16:52:38 -0700

    updated server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path')
 const favicon = require('serve-favicon')
 const logger = require('morgan')
 
+const app = express();
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
